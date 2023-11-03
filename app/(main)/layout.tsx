1469f1e64e2956a19392727f1391fa35cf31afed
kazoottt <yjwang.echo@gmail.com>
2023-11-03 18:23:58 +0800

    feat: new router group

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,12 @@
 import { FC, PropsWithChildren } from "react";
-import "./globals.css";
+import "@/app/globals.css";
 import StickyNavBar from "@/components/layout/stickyNavBar";
+import { ClerkProvider } from '@clerk/nextjs'
+
+export const metadata = {
+  title: "它像一颗",
+  description: "观测记录",
+};
 
 const RootLayout: FC<PropsWithChildren> = function ({ children }) {
   return (
